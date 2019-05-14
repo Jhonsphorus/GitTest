3390ee614a16ef2e5923afdb1ef68ae3cfc6a11e
unknown <b.adejohnson@hotmail.com>
2019-05-14 16:08:53 +0100

    Newly updated Book project

--- Book.java
+++ Book.java
@@ -1,9 +1,24 @@
 public class book{
     private String title;
     private String author;
-    private String year;
+    private int year;
     private int pages;
     private String color;
     private boolean isHardCover = true; 
+
+    // Creating setter methods
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    // Creating getter methods
+    public int getYear(){
+        return year;
+    }
+    public String getAuthor(){
+        return author;
+    } 
     
 }
