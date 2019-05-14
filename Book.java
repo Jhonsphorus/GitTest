a434ca577c69fdba12ecb392c7ba73325c5eef48
unknown <b.adejohnson@hotmail.com>
2019-05-14 15:02:05 +0100

    first commit

---
+++ Book.java
@@ -0,0 +1,9 @@
+public class book{
+    private String title;
+    private String author;
+    private String year;
+    private int pages;
+    private String color;
+    private boolean isHardCover = true; 
+    
+}
